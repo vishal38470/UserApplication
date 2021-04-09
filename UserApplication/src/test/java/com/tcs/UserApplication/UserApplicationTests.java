88b86fafda8dd594dc9c3ee5f6294e6505c1e064
saurabhparmar08\Owner <vishal.joshi.testing@gmail.com>
2021-04-09 11:42:19 +0530

    Base Project commit

---
+++ UserApplication/src/test/java/com/tcs/UserApplication/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.UserApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
