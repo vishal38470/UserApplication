ae7078d8c20797c8c1d71c5fabc440188c09b683
saurabhparmar08\Owner <vishal.joshi.testing@gmail.com>
2021-04-09 15:58:36 +0530

    hello-world3/hello API added in code

---
+++ UserApplication/src/main/java/com/tcs/UserApplication/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.tcs.UserApplication.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld { ;;
+
+	@GetMapping("/hello")
+	public String hello() {
+		
+		return "Hello bhai !!";; 
+	}
+}
